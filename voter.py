54f2b6f611323377b63b592a782169deac7cc735
Sabbir Ahmed Rony <160509348+Sabbir-Rony@users.noreply.github.com>
2025-03-02 23:48:39 +0600

    Add files via upload

---
+++ voter.py
@@ -0,0 +1,17 @@
+age = int(input("Enter the your age: "))
+
+if age == 18 :
+    print("Your are Voter")
+    print("Good Boy")
+
+elif 19 <= age <=50 :
+    print("Your are Young generation")
+
+elif 51 <= age <=80 :
+    print("Your are Old generation")
+
+elif age <=120 :
+    print("Your are too much old")
+
+else:
+    print("Your are not Human")
